94c084d0768b88aafa8479c4fc14ab209dd148bb
Mohammed Ehab <75863185+m7mdehab@users.noreply.github.com>
2024-04-23 05:17:22 +0000

    Refactor webapp with updated page structure and new documentation

--- pages/2_Oil_Spill_Detector.py
+++ pages/2_Oil_Spill_Detector.py
@@ -1,22 +1,17 @@
 import streamlit as st
 import os
+from PIL import Image
+import model_functions
 
 # Set environment variables to prevent TensorFlow from using GPU and TensorRT
 os.environ['CUDA_VISIBLE_DEVICES'] = ''  # Disable all GPUs visible to TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logs, only display errors
 
-from PIL import Image
-import model_functions
-
 # This should be the very first Streamlit command used in your app, and it should not be in a function
 st.set_page_config(page_title="Oil Spill Detection", page_icon="🌊")
 
 def main():
-    st.sidebar.write("## Explore Further")
-    st.sidebar.markdown("[Documentation](https://docs.streamlit.io)")
-    st.sidebar.markdown("[Community Forums](https://discuss.streamlit.io)")
-
-    st.write("# Welcome to our Oil Spill Detector! 🌊🛢️")
+    st.write("# Oil Spill Detector 🌊🛢️")
 
     uploaded_file = st.file_uploader("Choose a SAR image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
@@ -34,13 +29,11 @@ def main():
         # Button to run the model
         if st.button('Detect Oil Spill'):
             label = model_functions.predict(image, model_option)
-            st.write(f'Prediction: {label}')
 
-        st.markdown("""
-        ### More Information
-        - Want to learn more about how these models work? Check out [our documentation](https://docs.example.com).
-        - Have questions or feedback? Join our [community forums](https://discuss.example.com).
-        """)
+    # Add a message at the end of the page
+    st.markdown("""**Want to learn more?**""")
+    st.markdown("""Check out our [Our_Documentation](pages/3_Our_Documentation.py) for more in-depth information, detailed analysis of our results, and to learn more about our detection models.
+    """)
 
 if __name__ == "__main__":
     main()
